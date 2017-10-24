7340401b62922702b899813b771711fcc4254fc4
kurniawanhandito <kurniawanhandito@gmail.com>
2017-10-24 16:30:42 +0700

    add route resource

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/mahasiswa', function () {
-    return view('mahasiswa');
-});
+// Route::get('/mahasiswa', function () {
+//     return view('mahasiswa');
+// });
+
+Route::resource('mahasiswa','MahasiswaController');
